34f7b8a7ee5d8480f695aff5639707ed598b28c2
dzakia-st3 <105700671+dzakia-st3@users.noreply.github.com>
2022-09-19 09:51:52 +0700

    Update db_connections.js

--- helper/db_connections.js
+++ helper/db_connections.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2')
 const {host, user, password, database} = process.env
 const db = mysql.createPool({
+  connectionLimit : 100,
   host     : host,
   user     : user,
   password : password,
-  database : database
+  database : database,
+  debug : false
 })
 
 // db.connect((err)=> {
